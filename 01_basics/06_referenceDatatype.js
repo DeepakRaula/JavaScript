6c717a8db2d35b5624ccb552f9c8ac876ae1cc16
Deepak Raula <111013323+DeepakRaula@users.noreply.github.com>
2024-04-19 16:32:45 +0000

    referenceDatatypes

---
+++ 01_basics/06_referenceDatatype.js
@@ -0,0 +1,38 @@
+// reference data types(Non Primitive data type)
+
+/*
+
+Javascript is dynamically typed language
+
+1)Arrays
+2)Objects
+3)Functions
+
+*/
+
+// array
+const heros = ["Shaktiman","minaal murli","Hanu Maan"]
+
+// object declaration
+let myObj = {
+    UserName:"Deepak",
+    age:27,
+}
+
+
+//function declaration
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction); // function object
+myFunction()    // function call
+
+console.log(typeof heros);
+console.log(heros);  // object
+
+
+console.log(typeof myObj);
+console.log(myObj); // object
+ 
