ac8788d2c7773db240beab73229337d1caff9bdf
Deepak Raula <111013323+DeepakRaula@users.noreply.github.com>
2024-04-19 16:10:11 +0000

    javascript operations

---
+++ 01_basics/04_javascriptOperations.js
@@ -0,0 +1,61 @@
+// ************************** Operations ***************************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+/*
+Basic Numeric Operations
+
+1) +
+2) -
+3) *
+4) /
+5) **
+6) %
+7) ++
+8) --
+
+*/
+
+let s1 = "hello"
+let s2 = " Deepak"
+
+let s3 = s1 + s2;
+console.log(s3);
+
+// str + numeric datatype
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+// boolean + numeric data type
+
+console.log(1 + true);
+console.log(true + 1);
+console.log(false + 1);
+console.log(1+ false);
+console.log(false + false + 1);
+console.log(+true);
+
+
+// assignment operator
+let num1, num2, num3
+
+num1 = num2 = num3 = 4
+
+let gameCounter = 100
+console.log(gameCounter);
+
+gameCounter++;
+console.log(gameCounter);
+
+
+++gameCounter;
+console.log(gameCounter);
+
---
+++ 01_basics/05_comparisonOperations.js
@@ -0,0 +1,29 @@
+"use strict";
+//comparison operator
+
+/*
+1) ==       :	Returns true if the operands are equal.
+2) ===      :   Returns true if the operands are equal and of the same type.
+3) >        :   Returns true if the left operand is greater than the right operand.
+4) <        :   Returns true if the left operand is less than the right operand.
+5) >=       :   Returns true if the left operand is greater than or equal to the right operand.
+6) <=       :   Returns true if the left operand is less than or equal to the right operand.
+8) !=       :   Returns true if the operands are not equal.
+9) !==      :   Returns true if the operands are of the same type but not equal, or are of different type.
+
+
+*/
+ 
+// console.log(2 === '2');
+// console.log(2 == '2');
+
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+
+// console.log(undefined === null);
