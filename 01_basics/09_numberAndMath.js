b614e8903695528eb8cbef6360d1c370206f8044
Deepak Raula <111013323+DeepakRaula@users.noreply.github.com>
2024-04-20 18:44:43 +0000

    uncomenting code

--- 01_basics/09_numberAndMath.js
+++ 01_basics/09_numberAndMath.js
@@ -1,33 +1,35 @@
 // creating number data type
 
-// let a = 5
-// let b = 12
+let a = 5
+let b = 12
 
-// console.log(a);
-// console.log(b);
+console.log(a);
+console.log(b);
 
-// // creating number object
+// creating number object
 
-// let myNum = new Number(15.68723);
+let myNum = new Number(15.68723);
 
-// console.log(myNum);
+console.log(myNum);
 
-// console.log(myNum.toPrecision(5));
+console.log(myNum.toPrecision(5));
 
-// let myNum1 = new Number(165832681);
+let myNum1 = new Number(165832681);
 
 
-// console.log(myNum1.toLocaleString());  // international standard
-// console.log(myNum1.toLocaleString("en-IN"));  // indian standard
+console.log(myNum1.toLocaleString());  // international standard
+console.log(myNum1.toLocaleString("en-IN"));  // indian standard
 
 
 // *********************** MATH *******************************
 
-// console.log(Math.PI);
-// console.log(Math.abs(-4));
-// console.log(Math.round(5.67));
-// console.log(Math.ceil(5.67));
-// console.log(Math.floor(5.67));
+console.log(Math.PI);
+console.log(Math.abs(-4));
+console.log(Math.round(5.67));
+console.log(Math.ceil(5.67));
+console.log(Math.floor(5.67));
+
+
 console.log(Math.random());
 console.log((Math.random()*10) + 1 );
 
